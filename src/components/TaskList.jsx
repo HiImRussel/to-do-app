c6560320987db369d3dcc691a8efe15937f8c7e2
Russel <russel1234561@gmail.com>
2021-05-31 22:01:42 +0200

    Added filters

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 import Task from "./Task";
 import "../css/TaskList.css";
 
 const TaskList = ({ tasks, deleteTask, completeTask }) => {
+  const [filter, setFilter] = useState("new");
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+  };
+
   const display_tasks = tasks.map((task) => {
-    if (task.status === "new") {
+    if (task.status === filter) {
       return (
         <Task
           key={task.id}
@@ -18,7 +26,13 @@ const TaskList = ({ tasks, deleteTask, completeTask }) => {
   });
   return (
     <div className="tasks-list">
-      <h3>Task list</h3>
+      <div id="options">
+        <h3>Task list</h3>
+        <select onChange={handleChange}>
+          <option value="new">New</option>
+          <option value="complete">Completed</option>
+        </select>
+      </div>
       {display_tasks}
     </div>
   );
